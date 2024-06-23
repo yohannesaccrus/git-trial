f7181ea7546a2c5892fdf7398fc364566557956d
yohannes.accrus <yohanesaccrus014@gmail.com>
2024-06-23 21:43:03 +0700

    fix: add new about content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,12 @@ const About = () => {
     <div className="page">
       <h1>Git Trial</h1>
       <p>This is about page</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+        consequuntur nesciunt animi a earum, possimus ratione expedita quod
+        odit, facere vel nostrum. Doloribus minima numquam explicabo vero,
+        eveniet magni similique?
+      </p>
     </div>
   );
 };
