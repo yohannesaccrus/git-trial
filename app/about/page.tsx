6875ddf0025f5cb66f0c341f38e7a1ccbc67cdcd
yohannes.accrus <yohanesaccrus014@gmail.com>
2024-06-23 23:50:27 +0700

    fix: added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div className="page">
+      <h1>Git Trial</h1>
+      <p>This is about page</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+        consequuntur nesciunt animi a earum, possimus ratione expedita quod
+        odit, facere vel nostrum. Doloribus minima numquam explicabo vero,
+        eveniet magni similique?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+        corrupti voluptates deserunt tenetur eligendi nisi sint corporis, dolor
+        dignissimos, nostrum vitae. Tempora neque, id debitis harum ex corrupti
+        culpa possimus?
+      </p>
+    </div>
+  );
+};
+
+export default About;
