4d43bb0fc45397fec7b3043282b9959b95a405d1
yohannes.accrus <yohanesaccrus014@gmail.com>
2024-07-04 01:17:25 +0700

    fix: add about

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <div className="page">
+      <h1>Git About</h1>
+      <p>
+        When challenges arose, git merge brought their divergent paths back
+        together seamlessly. Amidst their journey, bugs occasionally tested
+        their resolve, but git log and git bisect empowered them to identify and
+        conquer these adversaries swiftly. United by git push, they synchronized
+        their efforts with remote collaborators, forging a collaborative
+        masterpiece that transcended individual contributions.
+      </p>
+    </div>
+  );
+};
+
+export default About;
